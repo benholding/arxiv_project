5e851fca3c6b0a030397f3c8c81e59efaeac7b7f
benholding <benjamin.holding@ki.se>
2022-10-05 15:34:19 +0200

    update to 2.

--- 2. getting author info.py
+++ 2. getting author info.py
@@ -5,8 +5,6 @@ Created on Wed Aug 31 14:10:08 2022
 
 @author: ben
 """
-import urllib.request
-import urllib
 import pandas as pd
 pd.set_option('display.max_columns', 6)
 
@@ -21,15 +19,9 @@ arxiv_id_df = article_citations_complete.index.to_numpy()
 
 doi_df = article_citations_complete.doi.reset_index().doi.to_csv("arxiv_published_dois_reduced.csv", index=False)
 
+#after doing the above, we extracted information from Web of Science by matching the dois to metadata in the database
 
+#%% import metadata about each published article
 
-#%% 
-arxiv_api_data = []
-url = 'http://export.arxiv.org/api/query?id_list=cond-mat/0207270v1'
-data = urllib.request.urlopen(url)
-print(data.read().decode('utf-8'))
-
-
-for i in len(arxiv_id_df)-1:
-    temp_df = urllib.request.urlopen("http://export.arxiv.org/api/query?id_list="+arxiv_id_df[i])
-    dataframes_list.append(data.read().decode('utf-8')))
+published_article_metadata = pd.read_csv("./data/wos_data/arxiv_pubs.csv")
+author_complete_publications = pd.read_csv("./data/wos_data/arxiv_clusterID_impact_frac.csv")
